7143163c6c22aa9ca4fabc5ebe2384096fa75043
Gabriel Ribeiro Rodrigues Dessotti <gabrieldessotti@hotmail.com>
2022-12-13 00:30:26 -0300

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,18 +1,50 @@
-section .data
-msg     db      'Jogo da Forca', 0AH
-len     equ     $-msg
+.data
+	msg_1: .asciiz "JOGO DA FORCA\n" #mensagem exibida
+	msg_2: .asciiz "Digite uma letra: " #mensagem exibida
+	msg_3: .asciiz "\n" #mensagem exibida
+	msg_4: .asciiz "Final de jogo\n" #mensagem exibida
+	
+	letra: .space 1
+	
+	palavra_1: .asciiz "casa"
+	palavra_2: .asciiz "bola"
+	palavra_3: .asciiz "arvore"
+	palavra_4: .asciiz "computador"
+	palavra_5: .asciiz "sapato"
+	
+.text
 
-
-section .text
-global  _start
-_start: mov     edx, len
-        mov     ecx, msg
-        mov     ebx, 1
-        mov     eax, 4
-        int     80h
- 
-        mov     ebx, 0
-        mov     eax, 1
-        int     80h
-
-        
+	li $v0, 4
+	la $a0, msg_1
+	syscall #so imprime a0 
+	
+	move $t0, $zero
+	
+	loop:
+		beq $t0, 5, saida
+		addi $t0, $t0, 1
+			
+		li $v0, 4
+		la $a0, msg_2
+		syscall #so imprime a0 
+		
+		li $v0, 8
+		la $a0, letra
+		la $a1, 1
+		syscall
+		
+		li $v0, 4
+		la $a0, msg_3
+		syscall #so imprime a0 
+		
+		la $a0, palavra_1
+		
+		j loop
+	
+	saida:
+	
+		li $v0, 4
+		la $a0, msg_3
+		syscall #so imprime a0 
+			
+		
